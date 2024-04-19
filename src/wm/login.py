ededbae20e9d3f33c696efef7760eb84a77f8dc3
DanielJ0131 <daniel.jonsson0131@stud.hkr.se>
2024-04-19 08:00:33 +0200

    Start with login.py

---
+++ src/wm/login.py
@@ -0,0 +1,11 @@
+"""Login module for the application."""
+
+from tkinter import Tk
+
+# window
+windows = Tk()
+windows.title("WellnessMate")
+windows.resizable(0, 0)
+windows.geometry("490x240+200+200")
+
+windows.mainloop()
