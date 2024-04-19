019516562b6db44f7d6bcbf1258d169c79c8bf34
DanielJ0131 <daniel.jonsson0131@stud.hkr.se>
2024-04-19 08:47:35 +0200

    Add first logo.png

--- src/wm/login.py
+++ src/wm/login.py
@@ -1,7 +1,6 @@
 """Login module for the application."""
 
 from tkinter import Frame, Label, Tk, PhotoImage
-from PIL import Image, ImageTk
 
 # Window
 windows = Tk()
@@ -14,7 +13,6 @@ frame = Frame(windows, width=700, height=400, bg='black')
 frame.place(x=0, y=0)
 
 logo_image = PhotoImage(file='src/assets/logo.png')
-user_label = Label(frame, text="Username", fg='#97ffff',
-                   font=("Arial", 12), bg='black', fg='white')
+user_label = Label(frame, text="Username", fg='white', bg='white')
 
 windows.mainloop()
