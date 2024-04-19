af54a6d9df145104d03ca623ac114e8199d3a2ae
DanielJ0131 <daniel.jonsson0131@stud.hkr.se>
2024-04-19 10:28:51 +0200

    Add

--- src/wm/login.py
+++ src/wm/login.py
@@ -1,54 +1,63 @@
 """Login module for the application."""
-
 from tkinter import Frame, Label, Tk, PhotoImage, Entry, Button
 
-# Window
-windows = Tk()
-windows.wm_title("WellnessMate")
-windows.wm_iconbitmap('src/assets/wd_logo.ico')
-windows.resizable(0, 0)
-windows.geometry("490x300")
 
-# Frame
-frame = Frame(windows, width=700, height=400, bg='black')
-frame.place(x=0, y=0)
+class Login:
+    """Login class for the application."""
 
-# Username
-user_image = PhotoImage(file='src/assets/user.png')
-user_label = Label(frame, text="Username: ", fg='#97ffff',
-                   image=user_image, compound='left', bg='black',
-                   font=('Calibre', 14))
-user_label.grid(row=1, column=0, padx=3, pady=20)
+    def __init__(self):
+        """Initializes the class."""
 
-# Password
-password_image = PhotoImage(file='src/assets/password.png')
-password_label = Label(frame, text="Password: ", fg='#97ffff',
-                       image=password_image, compound='left', bg='black',
-                       font=('Calibre', 14))
-password_label.grid(row=2, column=0, padx=3, pady=20)
+    def run():
+        """Runs the login window."""
 
-# Entries
-user_entry = Entry(frame, width=39, bd=3)
-user_entry.grid(row=1, column=2, columnspan=2, padx=57)
+        # Window
+        windows = Tk()
+        windows.wm_title("WellnessMate")
+        windows.wm_iconbitmap('src/assets/wd_logo.ico')
+        windows.resizable(0, 0)
+        windows.geometry("490x300")
 
-password_entry = Entry(frame, width=39, bd=3, show='*')
-password_entry.grid(row=2, column=2, columnspan=2)
+        # Frame
+        frame = Frame(windows, width=700, height=400, bg='black')
+        frame.place(x=0, y=0)
 
-# Login
-login_button = Button(frame, text="LOGIN", bg='#7f7fff', pady=10, width=23,
-                      fg='white', font=('Calibre', 14),
-                      cursor='hand2', border=0, borderwidth=5)
-login_button.grid(row=9, columnspan=5, pady=30)
+        # Username
+        user_image = PhotoImage(file='src/assets/user.png')
+        user_label = Label(frame, text="Username: ", fg='#97ffff',
+                           image=user_image, compound='left', bg='black',
+                           font=('Calibre', 14))
+        user_label.grid(row=1, column=0, padx=3, pady=20)
 
-# Create Account
-noaccount_label = Label(frame, text="No account?",
-                        fg='#97ffff', bg='black', font=('Calibre', 10),
-                        padx=4)
-noaccount_label.place(x=0, y=230)
+        # Password
+        password_image = PhotoImage(file='src/assets/password.png')
+        password_label = Label(frame, text="Password: ", fg='#97ffff',
+                               image=password_image, compound='left',
+                               bg='black', font=('Calibre', 14))
+        password_label.grid(row=2, column=0, padx=3, pady=20)
 
-create_button = Button(frame, text="Create Account", bg='white',
-                       font=('Calibre', 8), width=15,
-                       cursor='hand2', border=0)
-create_button.place(x=0, y=260)
+        # Entries
+        user_entry = Entry(frame, width=39, bd=3)
+        user_entry.grid(row=1, column=2, columnspan=2, padx=57)
 
-windows.mainloop()
+        password_entry = Entry(frame, width=39, bd=3, show='*')
+        password_entry.grid(row=2, column=2, columnspan=2)
+
+        # Login
+        login_button = Button(frame, text="LOGIN", bg='#7f7fff', pady=10,
+                              width=23, fg='white', font=('Calibre', 14),
+                              cursor='hand2', border=0, borderwidth=5)
+        login_button.grid(row=9, columnspan=5, pady=30)
+
+        # Create Account
+        noaccount_label = Label(frame, text="No account?",
+                                fg='#97ffff', bg='black', font=('Calibre', 10),
+                                padx=4)
+        noaccount_label.place(x=0, y=230)
+
+        create_button = Button(frame, text="Create Account", bg='white',
+                               font=('Calibre', 8), width=15,
+                               cursor='hand2', border=0)
+        create_button.place(x=0, y=260)
+
+        windows.mainloop()
--- src/wm/main.py
+++ src/wm/main.py
@@ -1,8 +1,12 @@
 """Main module for the application."""
 
+from login import Login  # type: ignore
+
 
 def main():
     """Is the main function of the application."""
+    login = Login()
+    login.run()
 
 
 if __name__ == "__main__":
