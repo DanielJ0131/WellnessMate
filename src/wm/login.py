02309e1f9327971c0ab63252fa4f003e3e0ef8c1
DanielJ0131 <daniel.jonsson0131@stud.hkr.se>
2024-04-19 08:41:24 +0200

    Add more login stuff

--- src/wm/login.py
+++ src/wm/login.py
@@ -1,11 +1,20 @@
 """Login module for the application."""
 
-from tkinter import Tk
+from tkinter import Frame, Label, Tk, PhotoImage
+from PIL import Image, ImageTk
 
-# window
+# Window
 windows = Tk()
 windows.title("WellnessMate")
 windows.resizable(0, 0)
 windows.geometry("490x240+200+200")
+
+# Frame
+frame = Frame(windows, width=700, height=400, bg='black')
+frame.place(x=0, y=0)
+
+logo_image = PhotoImage(file='src/assets/logo.png')
+user_label = Label(frame, text="Username", fg='#97ffff',
+                   font=("Arial", 12), bg='black', fg='white')
 
 windows.mainloop()
