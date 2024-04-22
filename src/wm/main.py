0d6c1fda8a75d57011cadf03a6b07d910aa0e9ea
Elina <rosatoelina@gmail.com>
2024-04-22 14:32:21 +0200

    refactored user interface to integrate all frames into the same window

--- src/wm/main.py
+++ src/wm/main.py
@@ -1,24 +1,11 @@
-"""Main module for the application."""
-
-from login import Login  # type: ignore
-from registration import Registration  # type: ignore
-from sport_events import SportEvents  # type: ignore
-
+from tkinter import Tk
+from user_interface import UserInterface
 
 def main():
-    """Is the main function of the application."""
-    print("1. Login")
-    print("2. Registration")
-    print("3. Sport Events")
-    print("4. Exit")
-    choice = int(input("Enter >>> "))
-    match (choice):
-        case 1: Login()
-        case 2: Registration()
-        case 3: SportEvents()
-        case 4: exit()
-        case _: print("Invalid choice!")
-
+    """Main function to launch the application."""
+    root = Tk()
+    app = UserInterface(root)
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
