b54da497286b234dda9a8ec8e5db4eb823cb82c3
André Sandblom <andre.sandblom0257@stud.hkr.se>
2024-04-26 21:40:45 +0200

    added create table in init class and flake8

--- app/src/database.py
+++ app/src/database.py
@@ -9,25 +9,34 @@ class Database:
         self.__password = "wellnessmate1234"
         try:
             self.db = pymysql.connect(
-                host=self.__host, user=self.__user, password=self.__password
+                host=self.__host,
+                user=self.__user,
+                password=self.__password,
+                database="wm_db"
             )
-            self.create_database()
+            self.create_database()  # Create database if it does not exist
             self.db.close()
             self.db = pymysql.connect(
-                host=self.__host, user=self.__user, password=self.__password, database="wm_db"
+                host=self.__host,
+                user=self.__user,
+                password=self.__password,
+                database="wm_db"
             )
+            self.create_tables()  # Create tables if they do not exist
             self.cur = self.db.cursor()
-        except pymysql.Error as e:
+        except pymysql.Error:
             print("Error: Could not connect to database.")
-
 
     def create_database(self):
         try:
-            self.cur.execute("CREATE SCHEMA IF NOT EXISTS `wm_db` DEFAULT CHARACTER SET utf8;")
+            self.cur.execute("""
+                CREATE SCHEMA IF NOT EXISTS `wm_db`
+                DEFAULT CHARACTER SET utf8;
+            """)
             self.db.commit()
         except pymysql.Error:
             print("Error: Could not create database.")
-    
+
     def create_tables(self):
         try:
             self.cur.execute("""
@@ -58,25 +67,24 @@ class Database:
                 ENGINE = InnoDB;
             """)
             self.db.commit()
-        except pymysql.Error as e:
+        except pymysql.Error:
             print("Error: Could not create tables.")
 
-
     def disconnect(self):
-        try: 
+        try:
             self.db.close()
-        except pymysql.Error as e:
+        except pymysql.Error:
             print("Error: Could not disconnect from database.")
 
     def query(self, query: str):
         try:
             self.cur.execute(query)
             self.db.commit()
-        except pymysql.Error as e:
+        except pymysql.Error:
             print("Error: Could not execute query.")
-    
+
     def commit(self):
         try:
             self.db.commit()
-        except pymysql.Error as e:
+        except pymysql.Error:
             print("Error: Could not commit changes to database.")
