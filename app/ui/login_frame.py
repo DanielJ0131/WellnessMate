bd1aa83a60c79388c0edd613d68c71165bf59fa7
Elina <rosatoelina@gmail.com>
2024-04-29 13:17:51 +0200

    updated Register Frame GUI with new design

--- app/ui/login_frame.py
+++ app/ui/login_frame.py
@@ -20,34 +20,29 @@ class LoginFrame(Frame):
 
     def create_widgets(self):
         """Create the widgets for the login frame."""
-        # Add login form elements here
         self.login_title = Label(self, text="Welcome back!", font=("Helvetica", 30, "bold"), fg="#2A2A28", bg="#F3F1EB")
         self.login_title.grid(row=0, column=0, pady=30, sticky='w')
 
         # Create a frame to input the username
         self.username_frame = Frame(self, bg="#F3F1EB")
         self.username_frame.grid(row=1, column=0, sticky='ew', pady=10)
-
-        self.username_label = Label(self.username_frame, text="Username:", font=("Helvetica", 16), bg="#F3F1EB", fg="#4C4A46")
+        self.username_label = Label(self.username_frame, text="Username:", font=("Helvetica", 14, "bold"), bg="#F3F1EB", fg="#4C4A46")
         self.username_label.grid(row=0, column=0, sticky='w')
         self.user_entry = Entry(self.username_frame, width=50, font=("Helvetica", 16), fg="#4C4A46", bg="#FFFFFF", relief="solid", highlightthickness=5, highlightbackground="#FFFFFF", borderwidth=0, insertbackground='#4C4A46')
-        self.user_entry.grid(row=1, column=0, sticky='ew', pady=10)
+        self.user_entry.grid(row=1, column=0, sticky='ew', pady=5)
 
-        # Create a frame to input the password
+        # Create a frame to input the password and a button to toggle password visibility
         self.pass_frame = Frame(self, bg="#F3F1EB")
         self.pass_frame.grid(row=2, column=0, sticky='ew', pady=10)
-
-        self.pass_label = Label(self.pass_frame, text="Password:", font=("Helvetica", 16), bg="#F3F1EB", fg="#4C4A46")
+        self.pass_label = Label(self.pass_frame, text="Password:", font=("Helvetica", 14, "bold"), bg="#F3F1EB", fg="#4C4A46")
         self.pass_label.grid(row=0, column=0, sticky='w')
         self.pass_entry = Entry(self.pass_frame, width=50, show="*", font=("Helvetica", 16), fg="#4C4A46", bg="#FFFFFF", relief="solid", highlightthickness=5, highlightbackground="#FFFFFF", borderwidth=0, insertbackground='#4C4A46')
-        self.pass_entry.grid(row=1, column=0, sticky='ew', pady=10)
-
-        # Create a button to toggle password visibility
+        self.pass_entry.grid(row=1, column=0, sticky='ew', pady=5)
         self.show_pass_button = Button(self.pass_frame, image=self.show_pass_image, bg="#D8B7E3", bd=0, highlightbackground="#F3F1E7", relief="solid", highlightthickness=0, borderwidth=0, command=self.toggle_password)
         self.show_pass_button.grid(row=1, column=1, sticky='ew', padx=5)
 
         # Create a button to submit data
-        self.submit_button = Button(self, text="Login", font=("Helvetica", 18), fg="#2A2A28", bg="#45B9AC", highlightbackground="#F3F1E7", relief="solid", highlightthickness=0, borderwidth=0, padx=20, pady=10, cursor="hand2", command=self.login)
+        self.submit_button = Button(self, text="Login", font=("Helvetica", 16, "bold"), fg="#2A2A28", bg="#45B9AC", highlightbackground="#F3F1E7", relief="solid", highlightthickness=0, borderwidth=0, padx=20, pady=10, cursor="hand2", command=self.login)
         self.submit_button.grid(row=3, column=0, sticky='w', pady=20)
 
     def toggle_password(self):
