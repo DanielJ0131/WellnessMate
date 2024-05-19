9936f2034925ba2cbc3786cbfa3914b6fe27cb38
Elina <rosatoelina@gmail.com>
2024-05-19 13:39:42 +0200

    changed window icon

--- app/main.py
+++ app/main.py
@@ -4,23 +4,14 @@ import os
 from tkinter import Tk, PhotoImage
 from ui.main_ui import MainUI
 from src.database import Database
-from platform import system
+
 
 def main():
     """Launch the main application."""
     db = Database()
     root = Tk()
-    platformD = system()
-    if platformD == 'Darwin':
-        logo_image = 'logo.icns'
-    else:
-        logo_image = 'logo.ico'
-    print(logo_image)
-
-
     MainUI(root, db)
-    root.iconbitmap(logo_image)
-
+    root.iconphoto(False, PhotoImage (file='app/assets/logo.png'))
     root.mainloop()
 
 
