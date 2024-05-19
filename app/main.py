9b9824fd97c3bea8585623831f357f432f720678
Elina <rosatoelina@gmail.com>
2024-05-19 14:21:18 +0200

    changed window icon

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 """Main module to launch the application."""
 
 import os
-from tkinter import Tk, PhotoImage
+import sys
+import ctypes
+from tkinter import Tk
 from ui.main_ui import MainUI
 from src.database import Database
 
@@ -11,20 +13,35 @@ def main():
     db = Database()
     root = Tk()
     MainUI(root, db)
-    root.iconphoto(False, PhotoImage (file='app/assets/icon.png'))
 
+    # Setting the icon in main window and taskbar
     icon_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'assets', 'logo.ico'))
-
-    # Check if the file exists and set the icon
     if os.path.exists(icon_path):
         try:
             root.iconbitmap(icon_path)
-            print("Icon set successfully.")
         except Exception as e:
             print(f"Error setting icon: {e}")
     else:
         print(f"Error: The file {icon_path} does not exist.")
+    set_taskbar_icon(root, icon_path)
+
     root.mainloop()
+
+def set_taskbar_icon(root, icon_path):
+    """Set the taskbar icon for Windows using ctypes."""
+    if sys.platform == 'win32':
+        try:
+            import ctypes
+            icon_path = os.path.abspath(icon_path)
+            if os.path.exists(icon_path):
+                icon = ctypes.windll.shell32.ExtractIconW(0, icon_path, 0)
+                ctypes.windll.user32.SendMessageW(root.winfo_id(), 0x80, 0, icon)
+            else:
+                print(f"Error: The file {icon_path} does not exist.")
+        except AttributeError as e:
+            print(f"Error setting taskbar icon: {e}")
+    else:
+        print("Taskbar icon setting is only supported on Windows.")
 
 
 if __name__ == "__main__":
